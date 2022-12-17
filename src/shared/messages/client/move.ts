239ecc8fe2c6049d407061fc799f2fe4109eeaed
ChillerDragon <chillerdragon@gmail.com>
2022-12-17 15:51:14 +0100

    beans

--- src/shared/messages/client/move.ts
+++ src/shared/messages/client/move.ts
@@ -1,1 +1,11 @@
 // direction: string
+
+// TODO: make this some kind of restricted string? enum?
+//       so it can only hold those values
+
+/*
+    left
+    right
+    up
+    down
+*/
--- src/shared/messages/client/username.ts
+++ src/shared/messages/client/username.ts
@@ -1,1 +1,6 @@
 // username: string
+
+// TODO: add length and charset validation into the type
+
+// maxlen: 32
+// charset: a-zA-Z0-9_
